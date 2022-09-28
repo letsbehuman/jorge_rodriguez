f305bab90ef7f5234d37555f74525e4aab4dc87f
letsbehuman <letsbehumandesign@gmail.com>
2022-09-28 11:39:06 +0200

    contact section complete

--- components/home/contact.js
+++ components/home/contact.js
@@ -3,6 +3,7 @@ import linkedin from '../../public/assets/social/linkedin.png';
 import codewars from '../../public/assets/social/codewars.png';
 import github from '../../public/assets/social/github.png';
 import Image from 'next/image';
+import CV from '../../public/assets/cv.js';
 
 const Contact = () => {
   return (
@@ -13,20 +14,26 @@ const Contact = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.socialSection}>
-          <div>Let's work together</div>
+          <h4>Let's work together</h4>
           <ul>
             <li className={styles.socialIcon}>
-              <a href="https://github.com/letsbehuman">
+              <a href="https://github.com/letsbehuman" target="_blank">
                 <Image src={github} layout="fill" objectFit="cover" />
               </a>
             </li>
             <li className={styles.socialIcon}>
-              <a href="https://www.codewars.com/users/letsbehuman">
+              <a
+                href="https://www.codewars.com/users/letsbehuman"
+                target="_blank"
+              >
                 <Image src={codewars} layout="fill" objectFit="cover" />
               </a>{' '}
             </li>
             <li className={styles.socialIcon}>
-              <a href="https://www.linkedin.com/in/jorgerodriguezvega-953564121/">
+              <a
+                href="https://www.linkedin.com/in/jorgerodriguezvega-953564121/"
+                target="_blank"
+              >
                 <Image src={linkedin} layout="fill" objectFit="cover" />
               </a>{' '}
             </li>
@@ -34,12 +41,16 @@ const Contact = () => {
         </div>
         <div className={styles.break}></div>
         <div className={styles.emailSection}>
-          <div>
-            <a href="">letsbehumandesign@gmail.com</a>
-          </div>
-          <div>
-            <a href="">My resume</a>
-          </div>
+          <h5>
+            <a href="mailto:letsbehumandesign@gmail.com">
+              letsbehumandesign@gmail.com
+            </a>
+          </h5>
+          <h5>
+            <a href={CV} download="Jorge Rodriguez CV">
+              My resume
+            </a>
+          </h5>
         </div>
       </div>
     </div>
