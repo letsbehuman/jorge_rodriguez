67dd2d928d255e8ee94cd5a61d02352c81079df2
letsbehuman <letsbehumandesign@gmail.com>
2022-09-30 16:29:49 +0200

    details to deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.scss';
-// import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Navbar /> */}
       <Component {...pageProps} />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import React, { Fragment } from 'react';
 import SocialBar from '../components/socialBar';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 import Landing from '../components/Landing';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import Collaboration from '../components/Collaboration';
 import About from '../components/About';
 import Contact from '../components/Contact';
